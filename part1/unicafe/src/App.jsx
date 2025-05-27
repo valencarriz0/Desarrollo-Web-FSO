61cd6918b3f440f73e1badc3e511e0625401ea06
valen carrizo <valentinacarrizo@gmail.com>
2025-05-27 19:33:02 -0300

    ejercicio 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return(
+    <div>
+      <h1>Give FeedBack</h1>
+      <button onClick={()=>{setGood(good + 1)}}>Bueno</button>
+      <button onClick={()=>{setNeutral(neutral + 1)}}>Neutral</button>
+      <button onClick={()=>{setBad(bad + 1)}}>Malo</button>
+      <h2>Estadísticas</h2>
+      <p>Bueno {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Malo {bad}</p>
+    </div>
+  )
+}
+
+export default App
