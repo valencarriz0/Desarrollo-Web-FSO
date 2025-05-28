fede3c898cdb7f4c00ef41c8fac3f917388d397a
valen carrizo <valentinacarrizo@gmail.com>
2025-05-27 21:02:14 -0300

    ejercicio 1.9

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 const Statistics = ({good, neutral, bad}) => {
-
-        const getAll= () => good+neutral+bad
-        const getAverage= () => getAll()===0 ? 0: (good - bad)/getAll()
-        const getPositive= ()=> getAll()===0 ? 0: (good/getAll())*100
+            const getAll= () => good+neutral+bad
+            const getAverage= () => getAll()===0 ? 0: (good - bad)/getAll()
+            const getPositive= ()=> getAll()===0 ? 0: (good/getAll())*100
 
   return (
-
+    getAll()===0 ? <p>No hay FeedBack</p> :
     <>
       <h2>Estadísticas</h2>
       <p>Bueno {good}</p>
@@ -16,8 +15,9 @@ const Statistics = ({good, neutral, bad}) => {
       <p>Todo {getAll()}</p>
       <p>Puntuación promedio {getAverage()}</p>
       <p>Porcentaje Positivo {getPositive()}%</p>
-      </>
+      
+    </>
   )
-}
 
+}
 export default Statistics
