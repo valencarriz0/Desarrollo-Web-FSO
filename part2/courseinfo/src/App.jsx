a3a2be46de123995ce9b554df29de210140d3238
valen carrizo <valentinacarrizo@gmail.com>
2025-06-17 20:15:00 -0300

    ejercicio 2.1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,34 +1,36 @@
-import Header from './components/header'
-import Content from './components/Content'
+import Course from './components/Course'
 import Total from './components/Total'
 const App = () => {
     const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Hola',
-        exercises: 12
-      }
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+        
     ]
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total  parts={course.parts}/>
+      {/*<Header course={course.name} />
+      <Content parts={course.parts}/>*/}
+      <Course course={course} />
+      
+
     </div>
   )
 }
--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -5,7 +5,7 @@ import Part from './Part'
 const Content = ( {parts}) => {
   return (
     <div>
-        {parts.map((part)=><Part name={part.name} exercises={part.exercises}/>)}
+        {parts.map((part)=><Part key={part.id} name={part.name} exercises={part.exercises}/>)}
     </div>
    
   )
