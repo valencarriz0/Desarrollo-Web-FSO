f8ece7cc233fe565cce1d3e9cf82bf86bd1963a7
valen carrizo <valentinacarrizo@gmail.com>
2025-06-17 19:40:02 -0300

    primer commit

---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Part from './Part'
+
+
+const Content = ( {parts}) => {
+  return (
+    <div>
+        {parts.map((part)=><Part name={part.name} exercises={part.exercises}/>)}
+    </div>
+   
+  )
+}
+
+export default Content
