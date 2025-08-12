3fa679ed141fa5c67c3f8015cbadd1cf3286736c
valen carrizo <valentinacarrizo@gmail.com>
2025-08-12 20:05:39 -0300

    ejercicio 2.6

---
+++ part2/Agenda/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [persons, setnpmPersons] = useState([{name:"Genaro"},{name: "Facundo"}])
+  const [newName, setNewName]= useState ('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+  return (
+      <div>
+        <h2>Agenda Telefónica</h2>
+        <form onSubmit={addName}>
+          <input value={newName} placeholder='ingrese nombre'/>
+          <button type="submit">Guardar</button>
+        </form> 
+        <h2>Numbers</h2>
+        <ul>{persons.map((person, id)=> <li key={id}>{person.name}</li> )}</ul>
+      </div>     
+  )
+}
+
+export default App
+
