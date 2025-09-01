77afff26e4abea8f8990db68274b9fa3d2a4130c
valen carrizo <valentinacarrizo@gmail.com>
2025-08-31 22:06:13 -0300

    ejercicio 2.8

--- part2/Agenda/src/App.jsx
+++ part2/Agenda/src/App.jsx
@@ -2,30 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [persons, setPersons] = useState([{name:"Genaro"},{name: "Facundo"}])
+  const [persons, setPersons] = useState([{name:"Genaro", number: "123456789"},{name: "Facundo", number: "987654321"}])
   const [newName, setNewName]= useState ('')
+  const [newNumber, setNewNumber]= useState ('')
 
-  const addName = (event) => {
+  const addNumber = (event) => {
     event.preventDefault()
-    console.log('newName', newName)
+    console.log('newNumber', newNumber)
 
       // Verifico si ya existe el nombre en la agenda
-    const nameExists = persons.some(
-      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    const numberExists = persons.some(
+      (person) => person.number === newNumber
     )
 
-    if (nameExists) {
-      alert(`${newName} ya está en la agenda telefónica`)
+    if (numberExists) {
+      alert(`${newNumber} ya está en la agenda telefónica`)
       return
     }
 
     const personObject = {
     name: newName,
+    number: newNumber,
     id: persons.length + 1,
     }
 
-  setPersons(persons.concat(personObject))
-  setNewName('')
+    setPersons(persons.concat(personObject))
+    setNewNumber('')
 
   }
 
@@ -34,15 +36,21 @@ function App() {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
       <div>
         <h2>Agenda Telefónica</h2>
-        <form onSubmit={addName}>
-          <input value={newName} placeholder='ingrese nombre' onChange={handleNameChange}/>
+        <form onSubmit={addNumber}>
+          <input value={newName} placeholder='Ingrese nombre' onChange={handleNameChange}/>
+          <input value={newNumber} placeholder='Ingrese número' onChange={handleNumberChange}/>
           <button type="submit">Guardar</button>
         </form> 
         <h2>Numbers</h2>
-        <ul>{persons.map((person)=> <li key={person.id}>{person.name}</li> )}</ul>
+        <ul>{persons.map((person)=> <li key={person.id}>{person.name} {person.number}</li> )}</ul>
       </div>     
   )
 }
