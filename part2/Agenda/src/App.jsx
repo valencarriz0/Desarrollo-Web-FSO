234de52dc8ec54a39fcf4392dd2c5a1be579e523
valen carrizo <valentinacarrizo@gmail.com>
2025-08-31 22:33:33 -0300

    ejercicio 2.9

--- part2/Agenda/src/App.jsx
+++ part2/Agenda/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [persons, setPersons] = useState([{name:"Genaro", number: "123456789"},{name: "Facundo", number: "987654321"}])
   const [newName, setNewName]= useState ('')
   const [newNumber, setNewNumber]= useState ('')
+  const [searchTerm, setSearchTerm] = useState('');
 
   const addNumber = (event) => {
     event.preventDefault()
@@ -40,18 +41,27 @@ function App() {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredItems = persons.filter(item =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
       <div>
+        <h3>Filtrar por nombre</h3>
+        <input type="text" placeholder="Buscar..." onChange={handleSearch}/>
         <h2>Agenda Telefónica</h2>
         <form onSubmit={addNumber}>
           <input value={newName} placeholder='Ingrese nombre' onChange={handleNameChange}/>
           <input value={newNumber} placeholder='Ingrese número' onChange={handleNumberChange}/>
           <button type="submit">Guardar</button>
-        </form> 
-        <h2>Numbers</h2>
-        <ul>{persons.map((person)=> <li key={person.id}>{person.name} {person.number}</li> )}</ul>
-      </div>     
+        </form>
+        <h2>Resultados de búsqueda</h2>
+        <ul>{filteredItems.map((person) => <li key={person.id}>{person.name} {person.number}</li>)}</ul>
+      </div>
   )
 }
 
